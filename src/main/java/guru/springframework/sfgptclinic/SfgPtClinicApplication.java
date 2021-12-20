fc4ba18630c76c7ddfb4942b50b052d3acc663cc
dawidrosa <dawidrosa@gmail.com>
2021-12-20 22:29:20 +0100

    initial commit

---
+++ src/main/java/guru/springframework/sfgptclinic/SfgPtClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgptclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPtClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPtClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/sfgptclinic/SfgPtClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgptclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPtClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
