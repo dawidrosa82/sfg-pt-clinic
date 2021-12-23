b74df49332b77314f231b2b5dcdccf4acca39f9d
dawidrosa <dawidrosa@gmail.com>
2021-12-23 21:14:21 +0100

    adding pojo. Close #1.

---
+++ src/main/java/guru/springframework/sfgptclinic/model/Owner.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgptclinic.model;
+
+import guru.springframework.sfgptclinic.model.Person;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/guru/springframework/sfgptclinic/model/Vet.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgptclinic.model;
+
+import guru.springframework.sfgptclinic.model.Person;
+
+public class Vet extends Person {
+}
